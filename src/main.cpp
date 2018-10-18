ea347982a824a925d0a39f688357bc0ab8e2f6c6
LAPTOP-F88O1O6K\chen <yiChen20180915@163.com>
2018-10-18 22:14:35 +0800

    glfw基础工程

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,30 @@
-﻿#include <QGuiApplication>
-#include <QtQuick/QQuickView>
+﻿#include <GLFW/glfw3.h>z
+#include <iostream>
+using namespace std;
 
-#include "triangle.h"
+void ErrorCallBack(int error, const char *description) {
 
-int main(int argc, char *argv[])
-{
-	//QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+}
 
-	QGuiApplication app(argc, argv);
+int main(int agrc, char *argv[]) {
 
-	qmlRegisterType<Triangle>("OpenGLUnderQML", 1, 0, "Triangle");
+	glfwSetErrorCallback(&ErrorCallBack);
+	if (glfwInit() != GL_TRUE) {
+		std::cerr << "glfw init error" << std::endl;
+		return -1;
+	}
+	GLFWwindow *window = nullptr;
+	if ((window = glfwCreateWindow(640, 480, "learn opengl", nullptr, nullptr)) == nullptr) {
+		std::cerr << "create window error" << std::endl;
+	}
+	glfwMakeContextCurrent(window);
+	while (glfwWindowShouldClose(window) != GL_TRUE) {
 
-	QQuickView view;
-	view.setResizeMode(QQuickView::SizeRootObjectToView);
-	view.setSource(QUrl(QStringLiteral("qrc:/ui/main.qml")));
-	view.show();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
-	return app.exec();
+	return 0;
 }
